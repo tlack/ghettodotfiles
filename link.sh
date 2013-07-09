f1cbf0c57ad29862b41177c424dd2392c425cae4
root <root@pop-friends.(none)>
2013-07-09 19:33:19 +0000

    script to link dotfilez

---
+++ link.sh
@@ -0,0 +1,14 @@
+dir=$(cd `dirname $0` && pwd)
+
+
+if [ -d $HOME/.vim ]; then
+	echo delete your $HOME/.vim
+	exit 1
+fi
+
+rm -rf $HOME/.vim $HOME/git_bash_prompt.sh
+
+ln -s $dir/.vim $HOME/.vim
+ln $dir/git_bash_prompt.sh $HOME/git_bash_prompt.sh
+echo ". $HOME/git_bash_prompt.sh" >> $HOME/.bashrc
+
