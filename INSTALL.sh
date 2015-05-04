8fb7c2dcc3f23cbf94d5a9c2ba8b50c37f975783
tlack <lackner@gmail.com>
2015-05-04 16:23:04 -0400

    prose -> code

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,7 +2,7 @@ dir=$(cd `dirname $0` && pwd)
 
 
 if [ -d $HOME/.vim -o -e $HOME/.vimrc -o -e $HOME/.ghettorc.sh ]; then
-	echo delete your $HOME/.vim $HOME/.vimrc $HOME/.ghettorc.sh
+	echo rm $HOME/.vim $HOME/.vimrc $HOME/.ghettorc.sh
 	exit 1
 fi
 
