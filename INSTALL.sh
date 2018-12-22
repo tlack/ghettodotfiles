8f7a8b4383ca6fbb400e0ef41b9754c9e8c28c1b
root <lackner@gmail.com>
2018-12-22 08:56:19 +0000

    remove annoying .vim presence check

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,12 +7,7 @@ echo ". $HOME/.ghettorc.sh" >> $HOME/.bashrc
 echo .screenrc
 ln $dir/screenrc $HOME/.screenrc
 
-if [ -d $HOME/.vim -o -e $HOME/.vimrc -o -e $HOME/.ghettorc.sh ]; then
-	echo wont touch existing vi - run   rm $HOME/.vim $HOME/.vimrc $HOME/.ghettorc.sh
-	exit 1
-fi
-
-rm -rf $HOME/.vim $HOME/ghettorc.sh
+rm -rf $HOME/.vim $HOME/.ghettorc.sh
 ln $dir/.vimrc $HOME/.vimrc
 ln -s $dir/.vim $HOME/.vim
 
