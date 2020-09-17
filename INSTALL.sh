bd1b79e85ab516bf7236a9b0ff79b6db4cf653bb
tlack <lackner@gmail.com>
2020-09-17 16:05:52 +0000

    adjust INSTALL to cope with ubuntu20

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,13 +1,16 @@
-dir=$(cd `dirname $0` && pwd)
+dir=$(dirname $0)
+echo ghettodotfiles dir = $dir
 
 echo .ghettorc 
+rm ~/.ghettorc.sh
 ln $dir/ghettorc.sh $HOME/.ghettorc.sh
 echo ". $HOME/.ghettorc.sh" >> $HOME/.bashrc
 
 echo .tmux.conf
+rm ~/.tmux.conf
 ln $dir/tmux.conf $HOME/.tmux.conf
 
-rm -rf $HOME/.vim $HOME/.ghettorc.sh
+rm -rf $HOME/.vim $HOME/.vimrc
 ln $dir/.vimrc $HOME/.vimrc
 ln -s $dir/.vim $HOME/.vim
 
