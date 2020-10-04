cb71f2522b31002b6fa714533b891d2059bc5726
tlack <lackner@gmail.com>
2020-10-04 16:57:46 -0400

    just use cp instead of ln -s

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,19 +3,19 @@ echo cwd = $dir
 
 echo Installing .ghettorc 
 rm ~/.ghettorc.sh 2>/dev/null
-ln $dir/ghettorc.sh $HOME/.ghettorc.sh
+cp $dir/ghettorc.sh $HOME/.ghettorc.sh
 echo ". $HOME/.ghettorc.sh" >> $HOME/.bashrc
 
 echo Installing .tmux.conf
 rm ~/.tmux.conf 2>/dev/null
-ln $dir/tmux.conf $HOME/.tmux.conf
+cp $dir/tmux.conf $HOME/.tmux.conf
 
 echo Installing .vim folder
 rm -rf $HOME/.vim $HOME/.vimrc 2>/dev/null
-ln -s $dir/.vim $HOME/.vim
+cp -Rp $dir/.vim $HOME/.vim
 
 echo Installing .vimrc
-ln $dir/.vimrc $HOME/.vimrc
+cp $dir/.vimrc $HOME/.vimrc
 
 echo Q: How do you make an octopus laugh?
 echo A: Give him ten-tickles
